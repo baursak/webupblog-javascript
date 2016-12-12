2f450c79e12637fa125180bc8c22466955044f46
baursak <baursak@gmail.com>
2016-12-12 18:51:20 +0300

    lesson 15: Math methods

---
+++ scratchpad-15.js
@@ -0,0 +1,7 @@
+var someStr = "my string";
+console.log(someStr.charAt(0));
+console.log("hello".indexOf("o"));
+console.log(someStr.toUpperCase());
+var someNum = 4.32;
+console.log(someNum.toFixed(4));
+var someBool = true;
